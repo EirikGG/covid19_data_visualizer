030b639beaab0d1b910b28e7ba64686845832a7b
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2020-11-02 18:00:54 +0100

    Did a lot, fucked up the style

--- covid_visualizer.py
+++ covid_visualizer.py
@@ -1,44 +1,33 @@
 import dash, json
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
-from covid_data import Regional_Data, Location_Data
+from datasets import Covid_Data
 
-def _wrap_fig(fig, title):
-        '''Wraps the figure in a div, card, cardbody, and a graph
-        https://stackoverflow.com/questions/63592900/plotly-dash-how-to-design-the-layout-using-dash-bootstrap-components'''
-        return html.Div([
-                dbc.Card(
-                        dbc.CardBody([
-                                html.H3(title),
-                                dcc.Graph(id=title, figure=fig)
-                        ])
-                )
-        ])
+def _format_array(arr):
+        '''Formats the array in array(dict(label: element), dict(label: element)). Used for dropdown menues'''
+        return [{'label': item, 'value':item} for item in arr]
 
-def _update_layout(fig):
-        '''Updates the figure layout'''
-        return fig#.update_layout(template='plotly_dark', 
-                  #              plot_bgcolor='rgba(0, 0, 0, 0)', 
-                   #             paper_bgcolor= 'rgba(0, 0, 0, 0)')
+def _update_fig(fig):
+        '''Updates layout for a figure'''
+        return fig.update_layout(template='plotly_dark', 
+                                plot_bgcolor='rgba(0, 0, 0, 0)', 
+                                paper_bgcolor= 'rgba(0, 0, 0, 0)')
 
-def _complete_fig(fig, title):
-        '''Takes a figure and updates the layout and wraps it in html tags'''
-        return _wrap_fig(_update_layout(fig), title)
 
 # Variable containg text used in website
 texts = json.load(open("text_content.json"))
 
 # Importing and setting theme
-external_stylesheets = [dbc.themes.LITERA]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+external_stylesheets = [dbc.themes.SLATE]
+app = dash.Dash("Covid-19 data visualizer", external_stylesheets=external_stylesheets)
 
 # Importing backends
-reg_data = Regional_Data()
-lo_data  = Location_Data()
+co_da = Covid_Data()
 
 
 
@@ -51,45 +40,15 @@ lo_data  = Location_Data()
 ##################### Figures #######################
 
 # Create regional data
-co_da = reg_data.get_total_cases()
+reg_summed = co_da.get_summed_cont()
 
 # Total cases by region (bar chart)
-total_by_region = px.bar(
-        pd.DataFrame({
-                "Continent": co_da.index, 
-                "Total infected": co_da["new_cases"]
-        }), 
-        x="Continent", 
-        y="Total infected"
-)
+total_by_region = go.Figure(data=(
+        go.Bar(name='Total cases', x=reg_summed.index, y=reg_summed["new_cases"]),
+        go.Bar(name='Total deaths', x=reg_summed.index, y=reg_summed["new_deaths"])))
 
 # Calculate lethality prosentage for each continent (pie chart)
-le_pr_cont_pie = px.pie(
-        pd.DataFrame({
-                "Continent": co_da.index,
-                "Lethality": co_da["new_cases"] 
-        }),
-        names="Continent", 
-        values="Lethality"
-)
-
-
-
-
-
-# Create location data
-lo_da = lo_data.get_location("Norway")
-
-# Trend data
-co_trend = px.line(
-        pd.DataFrame({
-                "Location": lo_da["date"],
-                "Total cases": lo_da["total_cases"]
-        }),
-        x="Location",
-        y="Total cases"
-)
-
+le_pr_cont_pie = go.Figure(data=go.Pie(labels=reg_summed.index, values=reg_summed["new_deaths"]))
 
 
 
@@ -103,30 +62,77 @@ app.layout = html.Div(children=[
                 dbc.Row([
                         dbc.Col(html.H1("Covid-19 data"))
                 ])
-        ]),
+        ], color="dark"),
 
+            
         dbc.Card(dbc.CardBody([
-                html.Br(),
-                
-                dbc.Row([
-                        dbc.Col(_complete_fig(total_by_region, "Total cases by continents"), width=7),
-                        dbc.Col(_complete_fig(le_pr_cont_pie, "Total cases compared"), width=5)
-                ], align='center'),
-                
-                html.Br(),
-                dbc.Row([
-                        dbc.Col(_complete_fig(co_trend, "Location"), width=12)
-                ], align='center'),
+                dcc.Tabs([
+                        dcc.Tab([
+                                html.Br(),
+                                dbc.Row([
+                                        dbc.Col(html.Div([
+                                                        dbc.Card(
+                                                                dbc.CardBody([
+                                                                        html.H3("Total cases and deaths by continents:"),
+                                                                        dcc.Graph(id='reg:comparison', figure=_update_fig(total_by_region))
+                                                                ])
+                                                        )
+                                                ]), width=7),
+                                        dbc.Col(html.Div([
+                                                        dbc.Card(
+                                                                dbc.CardBody([
+                                                                        html.H3("Death comparison:"),
+                                                                        dcc.Graph(id='reg:deaths', figure=_update_fig(le_pr_cont_pie))
+                                                                ])
+                                                        )
+                                                ]), width=5)
+                                ], align='center'),
+                                
+                                html.Br(),
+                                dbc.Row([
+                                        dbc.Col(
+                                                html.Div([
+                                                        dbc.Card(
+                                                                dbc.CardBody([
+                                                                        html.H3("Total cases for:"),
+                                                                        dcc.Dropdown(id='loc:trend_dropdown', 
+                                                                                options=_format_array(co_da.get_locations()), 
+                                                                                value=['Norway', 'Sweden', 'Denmark'], 
+                                                                                multi=True),
+                                                                        dcc.Graph(id='loc:trend_graph')
+                                                                ])
+                                                        )
+                                                ]), width=12)
+                                        ], align='center'),
+                        ], label="Regional data"),
+                        dcc.Tab([
+                                
+                        ], label="Temperature data")
+                ], colors={"primary":"dark",
+                        "background":"dark",
+                        "border":"black"}),
 
-                
                 html.Br(),
                 dbc.Card(dbc.CardBody([
                         html.P(texts["description"])
                 ]))
         ]))
-        
 ])
+
+
+
+@app.callback(
+        dash.dependencies.Output('loc:trend_graph', 'figure'),
+        [dash.dependencies.Input('loc:trend_dropdown', 'value')])
+def update_locations(locations):
+        '''Updates trend graph with one or several location trends'''
+        data = []
+        for location in (locations,) if str==type(locations) else locations:
+                lo_da = co_da.get_location(location)
+                data.append(dict(name=location, x=lo_da["date"], y=lo_da["total_cases"]))
+        return _update_fig(go.Figure(data = data))
+
 
 # Run webpage
 if __name__ == '__main__':
-        app.run_server(debug=True)
+        app.run_server(debug=True, use_reloader=True)
