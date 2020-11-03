09a5c5e0357740b5ccc269fb38a612b6b56e7f35
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2020-11-03 20:53:26 +0100

    Added temperature dataset

--- app.py
+++ app.py
@@ -5,12 +5,30 @@ import dash_html_components as html
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 
-from datasets import Covid_Data
+from sklearn import preprocessing
+
+from datasets import Covid_Data, Tmp_Data
 
 def _format_array(arr):
         '''Formats the array in array(dict(label: element), dict(label: element)). Used for dropdown menues'''
         return [{'label': item, 'value':item} for item in arr]
+
+def _get_common(col1, col2):
+        '''Takes twp collumns and returns common elemtents'''
+        return np.intersect1d(col1, col2)
+
+def _normalize(col):
+        '''Takes a pandas column and normalizes the values'''
+        # https://stackoverflow.com/questions/48823400/pandas-series-to-2d-array
+        col = pd.DataFrame(col.values.tolist())
+        
+        # https://chrisalbon.com/python/data_wrangling/pandas_normalize_column/
+        x = col.astype(float)                           # Creates a new array of floats 
+        min_max_scaler = preprocessing.MinMaxScaler()   # Creates a min/max object
+        x_scaled = min_max_scaler.fit_transform(x)      # Fit column
+        return pd.DataFrame(x_scaled)                   # Returns normalized result as df
 
 
 # Variable containg text used in website
@@ -23,7 +41,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 # Importing backends
-co_da = Covid_Data()
+co_da = Covid_Data()    # Covid data
+t_da = Tmp_Data()       # Temperature data
 
 
 
@@ -33,7 +52,7 @@ co_da = Covid_Data()
 
 
 
-##################### Figures #######################
+##################### Covid Figures #######################
 
 # Create regional data
 reg_summed = co_da.get_summed_cont()
@@ -52,6 +71,25 @@ le_pr_cont_pie = go.Figure(data=go.Pie(labels=reg_summed.index, values=reg_summe
 
 
 
+#####################  TMP Figures  #######################
+
+# Temperature for norway
+total_by_region = go.Figure(data=(
+        go.Bar(name='Total cases', x=reg_summed.index, y=reg_summed["new_cases"]),
+        go.Bar(name='Total deaths', x=reg_summed.index, y=reg_summed["new_deaths"])))
+
+
+
+
+
+
+#####################  Edu Figures  #######################
+
+
+
+
+
+
 # Webpage main layout
 app.layout = html.Div(children=[
         dbc.Navbar([
@@ -64,6 +102,7 @@ app.layout = html.Div(children=[
         dbc.Card(dbc.CardBody([
                 dcc.Tabs([
                         dcc.Tab([
+                                ##################### Covid Page #######################
                                 html.Br(),
                                 dbc.Row([
                                         dbc.Col(html.Div([
@@ -101,10 +140,42 @@ app.layout = html.Div(children=[
                                                 ]), width=12)
                                         ], align='center'),
                         ], label="Regional data"),
+
+
+
+
+
+
                         dcc.Tab([
-                                
+                                #####################  TMP Page  #######################
+                                html.Br(),
+                                dbc.Row([
+                                        dbc.Col(
+                                                html.Div([
+                                                        dbc.Card(
+                                                                dbc.CardBody([
+                                                                        html.H3("Total cases for:"),
+                                                                        dcc.Dropdown(id='tmp:trend_dropdown', 
+                                                                                options=_format_array(_get_common(co_da.get_locations(), t_da.get_locations())), 
+                                                                                value='Norway'),
+                                                                        dcc.Graph(id='tmp:trend_graph')
+                                                                ])
+                                                        )
+                                                ]), width=12)
+                                        ], align='center'),
                         ], label="Temperature data"),
+
+
+
+
+
+
+
+
+
+
                         dcc.Tab([
+                        #####################  Edu Page  #######################
                                 
                         ], label="Educational data")
                 ]),
@@ -128,6 +199,26 @@ def update_locations(locations):
                 lo_da = co_da.get_location(location)
                 data.append(dict(name=location, x=lo_da["date"], y=lo_da["total_cases"]))
         return go.Figure(data = data)
+
+
+
+
+@app.callback(
+        dash.dependencies.Output('tmp:trend_graph', 'figure'),
+        [dash.dependencies.Input('tmp:trend_dropdown', 'value')])
+def update_comp_tmp(location):
+        '''Updates trend graph with one or several location trends'''
+        
+        tm_da_co  = t_da.get_location(location)         # Tmp data for a single location
+
+        return go.Figure(data = (dict(name="Humidity", x=tm_da_co.index, y=tm_da_co["humidity"]), 
+                                dict(name="Max temperature[C]", x=tm_da_co.index, y=tm_da_co["maxtempC"]), 
+                                dict(name="Min temperature[C]", x=tm_da_co.index, y=tm_da_co["mintempC"])))
+
+
+
+
+
 
 
 # Run webpage
--- datasets.py
+++ datasets.py
@@ -2,58 +2,94 @@
 import pandas as pd
 import json, copy
 
-class Covid_Data(object):
-    '''Class can load and operate on covid-19 dataset from ourworld in data
-    https://covid.ourworldindata.org/data/owid-covid-data.csv. Class uses a
-    json file to load data.'''
-    
+class Data_Handler(object):
     paths       = None          # Paths and urls to find datasets
     covid_data  = None          # Covid-19 dataset
 
-    def __init__(self):
-        '''Poulates class fields'''
-        self.paths          = self._do_load_urls()
-        self.covid_data     = self._load_data(self.paths["covid-19"])
+    def __init__(self, url_key, url_path="config/dataset_paths.json"):
+        self.paths = self.do_load_urls(url_path)
+        self.data = self.load_data(self.paths[url_key])
 
-    def _load_data(self, paths):
+    def load_data(self, paths):
         '''Loads the data from the url array, if it getts an exeption 
         it tries the next one and so on'''
         res = None
         for path in paths:
             try: 
-                res = pd.read_csv(path) 
-                print("Loaded covid-19 dataset from {}".format(path))
+                res = pd.read_csv(path, parse_dates=True) 
+                print("Loaded dataset from {}".format(path))
                 break
             except Exception as e:
                 print("\nError loading dataset\n", e, "\nContinuing anyway...\n")
                 continue
         return res
 
-    def _do_load_urls(self, path="config/dataset_paths.json"):
+    def do_load_urls(self, path):
         '''Loads json file from path parameter'''
         return json.load(open(path))
-
-    def get_grouped_data(self, groups):
-        '''Returns pandas groupby object based on groups parameter'''
-        return 
 
     def get_urls(self):
         '''Returns a deepcopy of paths dict'''
         return copy.deepcopy(self.paths)
 
+
+class Covid_Data(Data_Handler):
+    '''Class can load and operate on covid-19 dataset from ourworld in data
+    https://covid.ourworldindata.org/data/owid-covid-data.csv. Class uses a
+    json file to load data.'''
+
+    def __init__(self, url_key="covid-19"):
+        '''Poulates class fields with the covid dataset'''
+        super().__init__(url_key)
+
+    def get_grouped_data(self, groups):
+        '''Returns pandas groupby object based on groups parameter'''
+        return 
+
     def get_summed_cont(self):
         '''Summed data for continent'''
-        return self.covid_data.groupby("continent").sum()
+        return self.data.groupby("continent").sum()
 
     def get_location(self, location):
         '''Get location data'''
-        return self.covid_data.groupby("location").get_group(location)
+        return self.data.groupby("location").get_group(location)
 
     def get_locations(self):
         '''Returns a column'''
-        return self.covid_data["location"].unique()
+        return self.data["location"].unique()
 
+class Tmp_Data(Data_Handler):
+    '''Handels temperature data from Kaggle
+    https://www.kaggle.com/ksudhir/weather-data-countries-covid19'''
+
+    def __init__(self, url_key="tmp"):
+        '''Poulates class fields with temperature data'''
+        super().__init__(url_key)
+
+    def get_location(self, location):
+        '''Returns the temparture data for a single location'''
+        row = self.data[self.data["Country/Region"]==location]
+
+        # Drops the location name set the temperature type as index and transpose
+        # which returns a df with parameter as column and date as index
+        return row.drop("Country/Region", 1).set_index("weather_param").transpose()
+
+    def get_locations(self):
+        '''Returns a column'''
+        return self.data["Country/Region"].unique()
 
 if __name__ == "__main__":
+    import numpy as np
+
     cd = Covid_Data()
-    
+    td = Tmp_Data()
+
+    cdl = cd.get_locations()
+    tdl = td.get_locations()
+
+    cl = np.intersect1d(cdl, tdl)
+
+    print(cl)
+    print("td: {}, cd: {}, cl: {}".format(len(cdl), len(tdl), len(cl)))
+
+    print(td.get_location("Norway"))
